2df1cb37e6c5ee4b18c6edbb89e52a707da75b3c
Coen Jacobs <coenjacobs@gmail.com>
2020-01-26 11:47:28 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const captureWebsite = require('capture-website')
+const app = express()
+const port = 3000
+
+app.get('/screenshot', (request, response) => {
+	let url = request.query.url;
+	let target = 'images/screenshot-' + Date.now().toString() + '.png';
+
+	captureWebsite.file(url,target, {
+		headers: {
+			'Cookie-Installing-Permission': 'all',
+		}
+	}).then(function(result) {
+  		response.json({
+  			'url': url,
+  			'screenshot': target,
+  		});
+	}, function(err) {
+		response.json({
+			'error': 'Something went wrong, you broke it',
+		});
+	});
+
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
